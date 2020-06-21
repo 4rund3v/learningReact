3aa16198704516ccf8ada8a0857c13be863b7ee5
arun dev <4rund3v@gmail.com>
2020-06-21 10:15:43 +0530

    attempting to build adashboard on material ui

--- oculus-dashboard/src/App.js
+++ oculus-dashboard/src/App.js
@@ -2,12 +2,20 @@ import React, { Component } from "react";
 
 // component imports
 import TopNavBar from "./components/topnavbar";
+import Sidebar from "./components/sidebar";
+import PersonalityLandingPage from "./components/personalities";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <TopNavBar />
+        <div className="container-fluid">
+          <div className="row">
+            <Sidebar />
+            <PersonalityLandingPage />
+          </div>
+        </div>
       </React.Fragment>
     );
   }
