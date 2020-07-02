5026a14fa0890f4bf4f528df433376839feb6737
arun dev <4rund3v@gmail.com>
2020-07-02 23:41:29 +0530

    updated apis tofetch from server

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import ListPersonalities from "./components/ListPersonalities";
-import { personalityData } from "./services/personalities";
+// import { personalityData } from "./services/personalities";
+import { getAll } from "./utils/ContactsApi";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { personalityData };
+    this.state = { personalityData: [] };
+  }
+  componentDidMount() {
+    console.log("Component Mounted ---> started fetch of personalities");
+    getAll().then((personalities) => {
+      this.setState({ personalityData: personalities });
+    });
   }
   render() {
     return (
--- contacts/src/utils/ContactsApi.js
+++ contacts/src/utils/ContactsApi.js
@@ -7,14 +7,19 @@ let token = "7e9bd56d9448";
 if (!token) token = localStorage.token = Math.random().toString(36).substr(-8);
 
 const headers = {
-  Accept: "application/json",
   client_id: token,
+  "Content-Type": "application/json",
 };
 
 export const getAll = () =>
-  fetch(`${api}/personalities`, { headers })
-    .then((res) => res.json())
-    .then((data) => data.personalities);
+  fetch(`${api}/personalities`, { method: "GET", headers })
+    .then(function (response) {
+      console.log(response);
+      return response.text().then(function (text) {
+        return text ? JSON.parse(text) : {};
+      });
+    })
+    .then((data) => data.personalities || []);
 
 export const remove = (contact) =>
   fetch(`${api}/personalities/${contact.id}`, { method: "DELETE", headers })
