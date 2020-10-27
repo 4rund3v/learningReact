9a43a97ec6157c4d4a1b5b8af2ef74f82a3715fd
arun dev <4rund3v@gmail.com>
2020-10-27 22:29:55 +0530

    updated to functional components.

--- counter-app/counter/src/components/counter.jsx
+++ counter-app/counter/src/components/counter.jsx
@@ -1,40 +1,35 @@
 import React, { Component } from "react";
 import { Button } from "react-bootstrap";
 
-class Counter extends Component {
-  state = {
-    id: this.props.prod.id,
-    name: this.props.prod.name,
-    value: this.props.prod.value,
-  };
+const getBadgeClasses = (value) => {
+  let spanClasses = "badge m-2 badge-";
+  spanClasses += value <= 0 ? "secondary" : "primary";
+  return spanClasses;
+};
 
-  styles = {
-    fontSize: 20,
-  };
+const styles = {
+  fontSize: 20,
+};
 
-  getBadgeClasses = (value) => {
-    let spanClasses = "badge m-2 badge-";
-    spanClasses += value <= 0 ? "secondary" : "primary";
-    return spanClasses;
-  };
-
-  render() {
-    const { id, name, value } = this.state;
-    return (
-      <React.Fragment>
-        {this.props.name}
-        <span style={this.styles} className={this.getBadgeClasses(value)}>
-          {this.state.value}
-        </span>
-        <Button variant="primary" onClick={() => this.props.onIncrement(id)}>
-          Increment
-        </Button>{" "}
-        <Button variant="danger" onClick={() => this.props.onDelete(id)}>
-          Delete
-        </Button>{" "}
-      </React.Fragment>
-    );
-  }
-}
+const Counter = (props) => {
+  console.log("Counter ", props);
+  const id = props.prod.id;
+  const value = props.prod.value;
+  const name = props.prod.name;
+  return (
+    <React.Fragment>
+      {name}
+      <span style={styles} className={getBadgeClasses(value)}>
+        {value}
+      </span>
+      <Button variant="primary" onClick={() => props.onIncrement(id)}>
+        Increment
+      </Button>{" "}
+      <Button variant="danger" onClick={() => props.onDelete(id)}>
+        Delete
+      </Button>{" "}
+    </React.Fragment>
+  );
+};
 
 export default Counter;
--- counter-app/counter/src/components/counters.jsx
+++ counter-app/counter/src/components/counters.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import { Button } from "react-bootstrap";
 import Counter from "./counter";
 
 class Counters extends Component {
@@ -13,6 +13,30 @@ class Counters extends Component {
       { name: "Joystick", value: 0, id: 6 },
     ],
   };
+
+  handleIncrement = (prodId) => {
+    console.log("handleIncrement", prodId);
+    //this.setState({ value: value + 1 });
+    const newProducts = this.state.products.map((prod) => {
+      if (prod.id === prodId) {
+        prod.value = prod.value + 1;
+        console.log(prod);
+      }
+      return prod;
+    });
+    console.log("Increment ", newProducts);
+    this.setState({ products: newProducts });
+  };
+
+  handleReset = () => {
+    console.log("HandleReset");
+    const newProducts = this.state.products.map((prod) => {
+      prod.value = 0;
+      return prod;
+    });
+    this.setState({ products: newProducts });
+  };
+
   handleDelete = (prodId) => {
     console.log("Delete Event Handler called.", prodId);
     const newProducts = this.state.products.filter(
@@ -21,18 +45,13 @@ class Counters extends Component {
     this.setState({ products: newProducts });
   };
 
-  handleIncrement = (prodId) => {
-    console.log("handleIncrement", prodId);
-    //this.setState({ value: value + 1 });
-    const newProducts = this.state.products.filter(
-      (prod) => prod.id !== prodId
-    );
-    this.setState({ products: newProducts });
-  };
-
   render() {
+    console.log("Rendering Data");
     return (
       <div>
+        <Button variant="primary" onClick={() => this.handleReset()}>
+          Reset
+        </Button>{" "}
         <ul>
           {this.state.products.map((prod) => (
             <li key={prod.id}>
@@ -42,7 +61,7 @@ class Counters extends Component {
                 prod={prod}
                 onIncrement={this.handleIncrement}
                 onDelete={this.handleDelete}
-              />
+              />{" "}
             </li>
           ))}
         </ul>
